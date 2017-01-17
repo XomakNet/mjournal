bf2989e28678be1512d62a61bccc63b1862bdfbf
Xomak <mail@xomak.net>
2017-01-17 23:20:33 +0300

    Added datetime picker, fixed frontend issues

--- main/forms.py
+++ main/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput
 
 from main.models import MaintenanceEvent
 
@@ -7,3 +7,6 @@ class EventForm(ModelForm):
     class Meta:
         model = MaintenanceEvent
         fields = ['maintenance_type', 'maintenance_date', 'comment']
+        widgets = {
+            'maintenance_date': TextInput(attrs={'id': 'maintenance_date_picker'}),
+        }
